03bc68efaf105fc83a9949ed2263ece417e48282
JohnnyOhms <officialjohn662@gmail.com>
2022-06-27 13:46:06 +0100

    updated from cloned remote

---
+++ main/script.js
@@ -0,0 +1,59 @@
+window.onload = ()=>{
+    displayList();
+}
+
+
+//variables
+const input = document.getElementById("text"),
+addBtn = document.querySelector(".add-btn"),
+listparent = document.querySelector(".todo-table");
+let task;
+
+addBtn.addEventListener('click', addList)
+function addList(ev){
+    ev.preventDefault();
+    if (input.value == ""){
+        return;
+    }else{
+        listparent.innerHTML = ""
+        AddToLocalstorage();
+        getFromLocalstorage();
+        displayList();
+    }
+
+}
+
+function AddToLocalstorage(){
+    if (localStorage.getItem("tasks") === null) {
+        task =[];
+    }else{
+        task = JSON.parse(localStorage.getItem("tasks"))
+    }
+    
+    task.push(input.value)
+    localStorage.setItem("tasks", JSON.stringify(task));
+    input.value = ""
+
+}
+
+function getFromLocalstorage(){
+  
+    task = JSON.parse(localStorage.getItem("tasks"))
+    console.log(task)
+}
+
+function displayList(){
+    getFromLocalstorage();
+    for (let i = 0; i < task.length; i++) {
+        const list = task[i];
+        let listElement = document.createElement('li');
+        listElement.innerHTML = list
+        listparent.appendChild(listElement);
+    }
+
+    // task.forEach(tasks => {
+    //     let listElement = document.createElement('li');
+    //     listElement.innerHTML = tasks
+    //     listparent.appendChild(listElement);
+    // });
+}
